c9261b91c28898c2a1428846abea9e444f69f988
Sinan Bolel <sinanbolel@gmail.com>
2013-03-10 00:33:02 -0600

    Added files

--- ARPong/ARPong.h
+++ ARPong/ARPong.h
@@ -1,9 +1,38 @@
 #pragma once
 
 #include <iostream>
-#include <gl/freeglut.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <GL/freeglut.h>
 #include <escapi.h>
 
 // The width and height we are going to work at.  ESCAPI automatically scales
 // larger images down to this.
 enum { WIDTH = 800, HEIGHT = 600 };
+
+
+// font strings for drawText2D();
+// (use as value for pFont argument)
+//-------------------------------
+#define rom10 GLUT_BITMAP_TIMES_ROMAN_10
+#define rom24 GLUT_BITMAP_TIMES_ROMAN_24
+#define helv10 GLUT_BITMAP_HELVETICA_10
+#define helv12 GLUT_BITMAP_HELVETICA_12
+#define helv18 GLUT_BITMAP_HELVETICA_18
+
+#define NU_PAUSE 10 // wait time (in milliseconds (mS)) that we use for GLUT's
+
+void glDisplay(void);                                 // GLUT display function
+void glKeyboard(unsigned char key, int xw, int yw);   // GLUT keyboard function
+void glKeySpecial(int key, int xw, int yw);           // GLUT special key function
+void runAnimTimer(int isOn);      // 0: Stop, 1: Resume
+void myTimer(int value);          // Used by runAnimTimer() to control animation
+void myHidden(int isVisible);     // Called by GLUT each time the display
+                                  // gets covered (visibility==0) or uncovered (==1)
+
+int getDisplayHeight(void) { return HEIGHT; }
+int getDisplayWidth(void)  { return WIDTH; }
+
+void drawText2D(void *pFont, double x0, double y0, const char *pString);
+
+
--- ARPong/main.cpp
+++ ARPong/main.cpp
@@ -1,4 +1,5 @@
 #include "ARPong.h"
+using namespace std;
 
 // Use the first webcam device we find.  Change this if we ever need to
 // support multiple devices.
@@ -7,27 +8,8 @@ enum { DEVICE = 0 };
 // Buffer into which video frames will be copied.
 int frame_buffer[WIDTH * HEIGHT];
 
-using namespace std;
-
-// OpenGL display function, called whenever a new frame is requested
-void display() {
-	// vertices for simple textured-quad rendering
-	const struct { float tu, tv; float x, y, z; } vertices[] = {
-		{ 0.0f, 0.0f, -1.0f,-1.0f, 0.0f },
-		{ 1.0f, 0.0f,  1.0f,-1.0f, 0.0f },
-		{ 1.0f, 1.0f,  1.0f, 1.0f, 0.0f },
-		{ 0.0f, 1.0f, -1.0f, 1.0f, 0.0f }
-	};
-
-	glPushMatrix();
-	glScaled(-1.0, -1.0, 0.0);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, WIDTH, HEIGHT, 0, GL_BGRA_EXT, GL_UNSIGNED_BYTE, frame_buffer);
-
-	glInterleavedArrays(GL_T2F_V3F, 0, vertices);
-	glDrawArrays(GL_QUADS, 0, 4);
-	glPopMatrix();
-	glutSwapBuffers();
-}
+static int nu_Anim_isOn = 1;        // ==1 to run animation, ==0 to pause.
+int isCleared = 1;                  // toggle animation's screen-clear
 
 // Set up the image capture library ESCAPI
 void setup_escapi() {
@@ -37,7 +19,6 @@ void setup_escapi() {
 		cerr << "ESCAPI found no devices.\n";
 		throw runtime_error("setupESCAPI");
 	}
-
 	// Tell the library where our buffer is and how big it is
 	SimpleCapParams params = { frame_buffer, WIDTH, HEIGHT };
 	if(!initCapture(DEVICE, &params)) {
@@ -65,15 +46,15 @@ bool main_loop_iter() {
 }
 
 int main(int argc, char** argv) {
-	setup_escapi();
+	// setup_escapi();
 
 	glutInit(&argc, argv);
 	glutInitWindowSize(WIDTH, HEIGHT);
 	glutCreateWindow("ARPong");
-	glutDisplayFunc(display);
+	glutDisplayFunc(glDisplay);
 
 	GLuint texture;
-	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+	glClearColor(0.5f, 0.0f, 0.0f, 1.0f);
 	glEnable(GL_TEXTURE_2D);
 	glViewport(0, 0, WIDTH, HEIGHT);
 	glGenTextures(1, &texture);
@@ -82,7 +63,119 @@ int main(int argc, char** argv) {
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
 	// keep running until the video source quits or someone closes us
-	while(main_loop_iter()) { }
+	// while(main_loop_iter()) { }
 
-	deinitCapture(DEVICE);
+	glutMainLoop();	                // enter GLUT's event-handler; NEVER EXITS.
+  runAnimTimer(1);                // start our animation loop.
+
+	// deinitCapture(DEVICE);
+}
+
+// OpenGL display function, called whenever a new frame is requested
+void glDisplay() {
+	// vertices for simple textured-quad rendering
+	const struct { float tu, tv; float x, y, z; } vertices[] = {
+		{ 0.0f, 0.0f, -1.0f,-1.0f, 0.0f },
+		{ 1.0f, 0.0f,  1.0f,-1.0f, 0.0f },
+		{ 1.0f, 1.0f,  1.0f, 1.0f, 0.0f },
+		{ 0.0f, 1.0f, -1.0f, 1.0f, 0.0f }
+	};
+
+	glPushMatrix();
+	glScaled(-1.0, -1.0, 0.0);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, WIDTH, HEIGHT, 0, GL_BGRA_EXT, GL_UNSIGNED_BYTE, frame_buffer);
+
+	glInterleavedArrays(GL_T2F_V3F, 0, vertices);
+	glDrawArrays(GL_QUADS, 0, 4);
+
+	glPopMatrix();
+
+	glFlush();	                // do any and all pending openGL rendering.
+	glutSwapBuffers();
+}
+
+
+void myKeySpecial(int key, int xw, int yw)
+//------------------------------------------------------------------------------
+{
+int xpos,ypos;      // mouse position in coords with origin at lower left.
+    xpos = xw;
+    ypos = getDisplayHeight() - yw; //(window system puts origin at UPPER left)
+	switch(key)
+	{
+        case GLUT_KEY_F1:       // DISPLAY HELP CONTENTS
+                cout << "Help File (F1 Key Pressed)\n";
+                glPushMatrix();
+                    glLoadIdentity();
+                    glScaled(0.2,0.2,0.2);
+                    glColor3d(0.0, 1.0, 0.0);	// And cyan-colored text on-screen:
+
+
+                    drawText2D(helv18,1.9, 3.0 +1.5,"HELP (Press F1 to Resume)");
+                    drawText2D(helv12,1.9, 2.85+1.5, "Press L mouse button to increase size");
+                    drawText2D(helv12,1.9, 2.7 +1.5, "Press R mouse button to decrease size");
+                    drawText2D(helv12,1.9, 2.55+1.5, "Press M mouse button to reset size");
+                    drawText2D(helv12,1.9, 2.4 +1.5, "Press R to toggle refreshing the screen");
+                    drawText2D(helv12,1.9, 2.25+1.5, "Press P to toggle the animation");
+                    drawText2D(helv12,1.9, 2.1+1.5,  "Press (.) to speed up the wheel clockwise");
+                    drawText2D(helv12,1.9, 1.95+1.5, "Press (,) to speed up the wheel counter-cw");
+                                if(nu_Anim_isOn ==1) runAnimTimer(0);
+                                else runAnimTimer(1);
+                glPopMatrix();
+                cout << "\nHELP FILE DISPLAYED!\n";
+            break;
+		default:
+			cout << "Special Key Pressed, Value: "<< (int)key << "\n)";
+			break;
+	}
+    glFlush();	        // do any and all pending openGL rendering.
+    glutSwapBuffers();	// For double-buffering: show what we drew.
+}
+
+void drawText2D(void *pFont, double x0, double y0, const char *pString)
+//-----------------------------------------------------------------------------
+//  Using the current color and specified font, write the text in 'pString' to
+// the display window.  Place the lower left corner of the first line of text
+//  at screen position x0,y0 in the current coordinate system.
+//      Example:  DrawText2D(helv18, 0.3,0.4 ,"Hello!");
+//
+// Available fonts:
+// helv10 (HELVETICA size 10)   rom10 (TIMES_ROMAN size 10)
+// helv12 (HELVETICA size 12)   rom24 (TIMES_ROMAN size 24)
+// helv18 (HELVETICA size 18)
+{
+int i, imax;					// character counters
+
+	glRasterPos2d(x0, y0);		// set text's lower-left corner position
+	imax = 1023;				// limit the number of chars we print.
+	for(i=0; pString[i] != '\0' && i<imax; i++)	// for each char,
+	{
+		glutBitmapCharacter(pFont, pString[i]);
+	}
+}
+
+void runAnimTimer(int isOn)
+//-----------------------------------------------------------------------------
+// Call runAnimTimer(0) to STOP animation;
+//      runAnimTimer(1) to START or CONTINUE it.
+{
+    nu_Anim_isOn = isOn;   // Update global var to the most-recent state;
+    if(isOn == 1)          // Start or continue animation?
+    {
+        glutTimerFunc(NU_PAUSE, myTimer, 1);
+    }
+}
+
+void myTimer (int value)
+//------------------------------------------------------------------------------
+{
+                                // GLUT finished waiting.
+    if(value != nu_Anim_isOn)
+        cout << "\n(Anim cancelled during GLUT wait)\n";
+    if(nu_Anim_isOn==1)         // Go on with animation?
+    {                           // YES.
+        glutPostRedisplay();    // tell GLUT to re-draw the screen, and
+        runAnimTimer(1);        // continue with the next time interval.
+    }
+                                // NO. Do nothing. No further animation.
 }
