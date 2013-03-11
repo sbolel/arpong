b518a28a0379d3b9f10573a133a550b0f74cde46
Steve Howard <steve@stevenhoward.org>
2013-03-11 16:21:06 -0500

    Forgot files

---
+++ ARPong/morph.cpp
@@ -0,0 +1,9 @@
+#include "morph.h"
+
+void erode(frame& img) {
+	for(int y0 = 1; y0 < HEIGHT - 1; ++y0) {
+		for(int x0 = 1; x0 < WIDTH - 1; ++x0) {
+		
+		}
+	}
+}
---
+++ ARPong/morph.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include "ARPong.h"
+
+void erode(frame& img);
